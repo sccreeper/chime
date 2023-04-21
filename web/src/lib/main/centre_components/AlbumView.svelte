94cff8b56d5b1dda0a84ea8dd0e0c19e729dd0c9
Oscar Peace <oscar@oscarcp.net>
2023-04-21 18:44:51 +0100

    Improve album view CSS

--- web/src/lib/main/centre_components/AlbumView.svelte
+++ web/src/lib/main/centre_components/AlbumView.svelte
@@ -2,11 +2,13 @@
     import { current_album, session_object } from "../../stores";
     import { get } from "svelte/store";
     import Track from "./Track.svelte";
+    import no_cover_image from "../../../assets/no_cover.png"
 
 
 let album_title = ""
 let album_cover_src = ""
 let tracks = []
+let is_album = false
 
 function updateView(album_id) {
     
@@ -30,16 +32,20 @@ current_album.subscribe(value => updateView(value));
 
 </script>
 
-<div class="inline">
-    <img src="" width="256" height="256"/>
-    <h1>{album_title}</h1>
+<div class="m-2">
+
+<div class="flex flex-row items-center gap-4">
+    <img src={album_cover_src == "" ? no_cover_image : album_cover_src} class="album-cover"/>
+    <h1 class="album-title float-right">{album_title}</h1>
 </div>
+
+<hr class="m-3 h-px border-none bg-gray-600">
 
 <div>
 
 {#if tracks.length == 0}
 
-<p>No tracks in album.</p>
+<p>No tracks in {is_album ? "album" : "playlist"}. Add from the lefthand sidebar or upload files.</p>
 
 {:else}
 
@@ -70,3 +76,26 @@ current_album.subscribe(value => updateView(value));
 
 </div>
 
+</div>
+
+<style>
+
+    .album-title { 
+
+        @apply text-6xl;
+        @apply text-gray-300;
+        @apply font-bold;
+
+    }
+
+    .album-cover {
+
+        width: 256px;
+        height: 256px;
+
+        min-height: 128px;
+        min-width: 128px;
+
+    }
+
+</style>
