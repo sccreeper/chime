eeb916c1e5c69e1173af3541b0428968aec5a1ce
Oscar Peace <oscar@oscarcp.net>
2023-06-16 23:42:50 +0100

    Add favicon to web app

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,11 +1,16 @@
 <script>
     import Login from "./lib/Login.svelte";
     import { view } from "./lib/stores";
+    import favicon from "./assets/logo.svg"
 
   console.log("Hello World from Chime!")
 
   view.set(Login)
 </script>
+
+<svelte:head>
+  <link rel="icon" type="image/svg" href={favicon}/>
+</svelte:head>
 
 <svelte:component this={$view}></svelte:component>
 
